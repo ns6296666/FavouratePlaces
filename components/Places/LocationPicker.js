d18b553bfe11e4235f5b161f59a90512abc2f66d
nidhisharma20 <ns6296666@gmail.com>
2024-04-02 14:51:57 +0100

    converting picked location into human readable address

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -7,7 +7,7 @@ import {
   useForegroundPermissions,
   PermissionStatus,
 } from "expo-location";
-import { mapPreview } from "../utils/location";
+import { getAddress, mapPreview } from "../utils/location";
 import {
   useNavigation,
   useRoute,
@@ -34,7 +34,16 @@ function LocationPicker({ onPickLocation }) {
   }, [route, isFocused]);
 
   useEffect(() => {
-    onPickLocation(pickedLocation);
+    async function handleLocation() {
+      if (pickedLocation) {
+        const address = await getAddress(
+          pickedLocation.lat,
+          pickedLocation.lng
+        );
+        onPickLocation({ ...pickedLocation, address: address });
+      }
+    }
+    handleLocation();
   }, [pickedLocation, onPickLocation]);
   async function verifyPermission() {
     if (
--- components/utils/location.js
+++ components/utils/location.js
@@ -1,6 +1,18 @@
-GOOGLE_API_KEY = "AIzaSyA5COqGZTT7a0H7owWN5W-aTDoHxln5ThQ";
+GOOGLE_API_KEY = "AIzaSyDmRv6NajQE0bSPkgClZyJrp_kBjPgDixM";
 
 export function mapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:blue%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
   return imagePreviewUrl;
+}
+
+export async function getAddress(lat, lng) {
+  const Url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLE_API_KEY}`;
+  const result = await fetch(Url);
+  if (!result.ok) {
+    throw new Error("Failed to fetch address");
+  }
+  const data = await result.json();
+  const address = data.results[0].formatted_address;
+  return address;
+  console.log("address", address.results[0].formatted_address);
 }
